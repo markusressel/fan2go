723ff44feeeb13b0f64421a154aef7421fec2b78
Markus Ressel <mail@markusressel.de>
2021-09-29 23:36:15 +0200

    v0.0.18

--- cmd/root.go
+++ cmd/root.go
@@ -170,7 +170,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of fan2go",
 	Long:  `All software has versions. This is fan2go's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ui.Println("0.0.17")
+		ui.Println("0.0.18")
 	},
 }
 
