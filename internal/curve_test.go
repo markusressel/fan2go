d30e188c701fdb29400201f9f802768a67cb5de8
Markus Ressel <mail@markusressel.de>
2021-11-01 00:56:40 +0100

    fixed tests

--- internal/curve_test.go
+++ internal/curve_test.go
@@ -7,26 +7,39 @@ import (
 
 func TestFunctionCurve(t *testing.T) {
 	// GIVEN
+	avgTmp := 60000.0
+	sensor := Sensor{
+		Name:  "sensor",
+		Label: "Test",
+		Index: 1,
+		Input: "test",
+		Config: &SensorConfig{
+			Id:       "sensor",
+			Platform: "platform",
+			Index:    1,
+		},
+		MovingAvg: avgTmp,
+	}
+	SensorMap[sensor.Config.Id] = &sensor
+
 	function := "average"
 	curves := []CurveConfig{
 		{
 			Id:   "case_fan_front",
 			Type: "linear",
-			Params: LinearCurveConfig{
-				MinTemp: 40,
-				MaxTemp: 255,
-				Steps: map[int]int{
-					10:  10,
-					255: 255,
-				},
+			Params: map[string]interface{}{
+				"Sensor":  "sensor",
+				"MinTemp": 40,
+				"MaxTemp": 60,
 			},
 		},
 		{
 			Id:   "case_fan_back",
 			Type: "linear",
-			Params: LinearCurveConfig{
-				MinTemp: 40,
-				MaxTemp: 255,
+			Params: map[string]interface{}{
+				"Sensor":  "sensor",
+				"MinTemp": 40,
+				"MaxTemp": 60,
 			},
 		},
 	}
@@ -44,7 +57,7 @@ func TestFunctionCurve(t *testing.T) {
 	result := FunctionCurve(function, curveValues)
 
 	// THEN
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 255, result)
 }
 
 func TestLinearCurve(t *testing.T) {
@@ -62,15 +75,15 @@ func TestLinearCurve(t *testing.T) {
 		},
 		MovingAvg: avgTmp,
 	}
-
 	SensorMap[sensor.Config.Id] = &sensor
+
 	config := CurveConfig{
 		Id:   "curve",
 		Type: LinearCurveType,
-		Params: LinearCurveConfig{
-			Sensor:  sensor.Config.Id,
-			MinTemp: 40,
-			MaxTemp: 80,
+		Params: map[string]interface{}{
+			"Sensor":  sensor.Config.Id,
+			"MinTemp": 40,
+			"MaxTemp": 80,
 		},
 	}
 
