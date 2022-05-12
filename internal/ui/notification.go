aa70b82b7fd34d93e276d121a84a4ee042b0ca2c
Markus Ressel <mail@markusressel.de>
2022-05-12 02:38:31 +0200

    added custom implementation for sending notifications via notify-send

--- internal/ui/notification.go
+++ internal/ui/notification.go
@@ -1,7 +1,9 @@
 package ui
 
 import (
-	"github.com/mqu/go-notify"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 // For a list of possible icons, see: https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
@@ -10,10 +12,6 @@ const (
 	IconDialogInfo  = "dialog-information"
 	IconDialogWarn  = "dialog-warning"
 )
-
-func init() {
-	notify.Init("fan2go")
-}
 
 func NotifyInfo(title, text string) {
 	NotifySend(title, text, IconDialogInfo)
@@ -28,8 +26,46 @@ func NotifyError(title, text string) {
 }
 
 func NotifySend(title, text, icon string) {
-	hello := notify.NotificationNew(title, text, icon)
-	err := hello.Show()
+	display, exists := os.LookupEnv("DISPLAY")
+	if !exists {
+		Warning("Cannot send notification, missing env variable 'DISPLAY'!")
+		return
+	}
+
+	cmd := exec.Command("who")
+	output, err := cmd.Output()
+	if err != nil {
+		Warning("Cannot send notification, unable to find user of display session: %v", err)
+		return
+	}
+	lines := strings.Split(string(output), "\n")
+	var user string
+	for _, line := range lines {
+		if strings.Contains(line, display) {
+			user = strings.TrimSpace(strings.Fields(line)[0])
+			break
+		}
+	}
+
+	if len(user) <= 0 {
+		Warning("Cannot send notification, unable to detect user of current display session")
+		return
+	}
+
+	cmd = exec.Command("id", "-u", user)
+	output, err = cmd.Output()
+	userIdString := strings.TrimSpace(string(output))
+	if len(userIdString) <= 0 {
+		Warning("Cannot send notification, unable to detect user id")
+		return
+	}
+
+	cmd = exec.Command("sudo", "-u", user,
+		"DISPLAY="+display,
+		"DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/"+userIdString+"/bus",
+		"notify-send", "-i", icon, title, text,
+	)
+	err = cmd.Run()
 	if err != nil {
 		Error("Error sending notification: %v", err)
 	}
