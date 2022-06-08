553dece059312c8423c30976e5a34ab5cdda1583
Markus Ressel <mail@markusressel.de>
2022-06-08 16:53:41 +0200

    move neverStop warning to controller

--- internal/fans/hwmon.go
+++ internal/fans/hwmon.go
@@ -42,10 +42,6 @@ func (fan HwMonFan) GetMinPwm() int {
 	// if the fan is never supposed to stop,
 	// use the lowest pwm value where the fan is still spinning
 	if fan.ShouldNeverStop() {
-		if len(fan.Config.HwMon.RpmInput) <= 0 {
-			ui.Warning("WARN: cannot guarantee neverStop option on fan %s, since it has no RPM input.", fan.GetId())
-		}
-
 		var minPwm int
 		if fan.Config.MinPwm != nil {
 			minPwm = *fan.Config.MinPwm
