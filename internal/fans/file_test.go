6199d809eede72ef178de77fdd53495e261c745a
Markus Ressel <mail@markusressel.de>
2024-09-21 14:40:32 +0200

    added tests

--- internal/fans/file_test.go
+++ internal/fans/file_test.go
@@ -6,18 +6,36 @@ import (
 	"testing"
 )
 
+func TestFileFan_NewFan(t *testing.T) {
+	// GIVEN
+	id := "test"
+	config := configuration.FanConfig{
+		ID: id,
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
+		},
+	}
+
+	// WHEN
+	fan, err := NewFan(config)
+
+	// THEN
+	assert.NoError(t, err)
+	assert.NotNil(t, fan)
+}
+
 func TestFileFan_GetId(t *testing.T) {
 	// GIVEN
 	id := "test"
 	config := configuration.FanConfig{
 		ID: id,
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	result := fan.GetId()
@@ -29,12 +47,11 @@ func TestFileFan_GetStartPwm(t *testing.T) {
 	// GIVEN
 	config := configuration.FanConfig{
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	result := fan.GetStartPwm()
@@ -47,13 +64,12 @@ func TestFileFan_SetStartPwm(t *testing.T) {
 	// GIVEN
 	config := configuration.FanConfig{
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
 
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	fan.SetStartPwm(100, false)
@@ -67,13 +83,12 @@ func TestFileFan_GetMinPwm(t *testing.T) {
 	// GIVEN
 	config := configuration.FanConfig{
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
 
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	result := fan.GetMinPwm()
@@ -86,13 +101,12 @@ func TestFileFan_SetMinPwm(t *testing.T) {
 	// GIVEN
 	config := configuration.FanConfig{
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
 
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	fan.SetMinPwm(100, false)
@@ -106,13 +120,12 @@ func TestFileFan_GetMaxPwm(t *testing.T) {
 	// GIVEN
 	config := configuration.FanConfig{
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
 
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	result := fan.GetMaxPwm()
@@ -125,13 +138,12 @@ func TestFileFan_SetMaxPwm(t *testing.T) {
 	// GIVEN
 	config := configuration.FanConfig{
 		File: &configuration.FileFanConfig{
-			Path:    "/path/to/pwm",
-			RpmPath: "/path/to/rpm",
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
 		},
 	}
 
-	fan, err := NewFan(config)
-	assert.NoError(t, err)
+	fan, _ := NewFan(config)
 
 	// WHEN
 	fan.SetMaxPwm(100, false)
@@ -139,4 +151,145 @@ func TestFileFan_SetMaxPwm(t *testing.T) {
 	// THEN
 	// NOTE: file fan does not support setting max pwm
 	assert.Equal(t, 255, fan.GetMaxPwm())
+}
+
+func TestFileFan_GetRpm(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
+		},
+	}
+
+	fan, _ := NewFan(config)
+
+	// WHEN
+	result, err := fan.GetRpm()
+
+	// THEN
+	assert.NoError(t, err)
+	assert.Equal(t, 2150, result)
+}
+
+func TestFileFan_GetRpm_InvalidPath(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/non_existent_file",
+			RpmPath: "../../test/non_existent_file",
+		},
+	}
+
+	fan, _ := NewFan(config)
+
+	// WHEN
+	result, err := fan.GetRpm()
+
+	// THEN
+	assert.Error(t, err)
+	assert.Equal(t, 0, result)
+}
+
+func TestFileFan_SetRpmAvg(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/non_existent_file",
+			RpmPath: "../../test/non_existent_file",
+		},
+	}
+
+	fan, _ := NewFan(config)
+
+	rpmAvg := 1000.5
+
+	// WHEN
+	fan.SetRpmAvg(rpmAvg)
+
+	// THEN
+	assert.Equal(t, float64(int(rpmAvg)), fan.GetRpmAvg())
+}
+
+func TestFileFan_GetRpmAvg(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/non_existent_file",
+			RpmPath: "../../test/non_existent_file",
+		},
+	}
+
+	fan, _ := NewFan(config)
+
+	rpmAvg := 1234.5
+	fan.SetRpmAvg(rpmAvg)
+
+	// WHEN
+	result := fan.GetRpmAvg()
+
+	// THEN
+	assert.Equal(t, float64(int(rpmAvg)), result)
+}
+
+func TestFileFan_GetPwm(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
+		},
+	}
+
+	fan, _ := NewFan(config)
+
+	// WHEN
+	result, err := fan.GetPwm()
+
+	// THEN
+	assert.NoError(t, err)
+	assert.Equal(t, 152, result)
+}
+
+func TestFileFan_GetPwm_InvalidPath(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/non_existent_file",
+			RpmPath: "../../test/non_existent_file",
+		},
+	}
+
+	fan, _ := NewFan(config)
+
+	// WHEN
+	result, err := fan.GetPwm()
+
+	// THEN
+	assert.Error(t, err)
+	assert.Equal(t, 0, result)
+}
+
+func TestFileFan_SetPwm(t *testing.T) {
+	// GIVEN
+	config := configuration.FanConfig{
+		File: &configuration.FileFanConfig{
+			Path:    "../../test/file_fan_pwm",
+			RpmPath: "../../test/file_fan_rpm",
+		},
+	}
+
+	fan, _ := NewFan(config)
+	targetPwm := 100
+
+	// WHEN
+	err := fan.SetPwm(targetPwm)
+
+	// THEN
+	assert.NoError(t, err)
+
+	result, err := fan.GetPwm()
+
+	assert.NoError(t, err)
+	assert.Equal(t, targetPwm, result)
 }
