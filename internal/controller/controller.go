c33af4dd95bd380aa918bf0be0f77bed7f82ade1
Markus Ressel <mail@markusressel.de>
2025-03-24 22:58:50 +0100

    revert: fix setting wrong value to "lastSetPwm"

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -511,7 +511,7 @@ func (f *DefaultFanController) setPwm(target int) (err error) {
 	closestExpected := f.applyPwmMapping(closestTarget)
 
 	ui.Debug("Setting PWM of %s to %d, found closest PWM distinct value at %d, applying PWM Map yields %d", f.fan.GetId(), target, closestTarget, closestExpected)
-	f.lastSetPwm = &closestTarget
+	f.lastSetPwm = &target
 	// if we can read the PWM value, we can check if the fan is already at the target value
 	// and avoid unnecessary setPwm calls
 	if f.fan.Supports(fans.FeaturePwmSensor) {
